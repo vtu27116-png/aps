9468241d8d33c6e3e64308b0e78d5bdab10c3e6e
vtu27116-png <vtu27116@veltech.edu.in>
2026-02-02 22:30:47 +0530

    Time: 2 ms (90.02%) | Memory: 48 MB (14.74%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+     
+        int lastNonZeroFoundAt = 0;
+
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt] = nums[i];
+                lastNonZeroFoundAt++;
+            }
+        }
+
+        
+        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
+  
+    
