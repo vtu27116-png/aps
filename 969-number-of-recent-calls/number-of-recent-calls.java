e9fd0ef904211be25fd4096021892c92fc3c436a
vtu27116-png <vtu27116@veltech.edu.in>
2026-03-24 09:50:29 +0530

    Time: 24 ms (15.55%) | Memory: 59.7 MB (29.82%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class RecentCounter {
+    Queue<Integer> q = new LinkedList<>();
+    public RecentCounter() {}
+    public int ping(int t) {
+        q.offer(t);
+        while (q.peek() < t - 3000) q.poll(); 
+        return q.size();
+    }
+}
