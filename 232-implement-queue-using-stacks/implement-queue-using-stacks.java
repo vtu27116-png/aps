f814eb9db7ab534be1270869a9f68c60835b494d
vtu27116-png <vtu27116@veltech.edu.in>
2026-03-24 09:37:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (71.71%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,34 +1,49 @@
-
-
 class MyQueue {
-    private Stack<Integer> s1;
-    private Stack<Integer> s2;
-
+     
+     Stack<Integer> s1,s2;
     public MyQueue() {
         s1 = new Stack<>();
         s2 = new Stack<>();
     }
-
+    
     public void push(int x) {
-        while (!s1.isEmpty()) {
-            s2.push(s1.pop());
-        }
         s1.push(x);
-        while (!s2.isEmpty()) {
-            s1.push(s2.pop());
-        }
+        
     }
-
+    
     public int pop() {
-        return s1.pop();
-    }
+        
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            
+        }
+        if(s2.isEmpty()){
+            return -1;
+        }
 
+        int topelement = s2.pop();
+        return topelement;
+    }
+    
     public int peek() {
-        return s1.peek();
-    }
 
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+            s2.push(s1.pop());
+            }
+
+        }
+
+        int topelement = s2.peek();
+        return topelement;
+        
+    }
+    
     public boolean empty() {
-        return s1.isEmpty();
+        return s1.isEmpty() && s2.isEmpty();
+        
     }
 }
 
